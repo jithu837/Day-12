00e5f7e0870f6e98e5802a193e77dd073c0cd1d2
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-13 14:08:25 +0530

    Add files via upload

---
+++ PutHashMap.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class PutHashMap{
+    public static void main(String args[])
+    {
+        HashMap<String,String> a= new HashMap<>();
+        a.put("SNO","01");
+        a.put("SName","Muraja");
+        System.out.println(a);
+    }
+}
